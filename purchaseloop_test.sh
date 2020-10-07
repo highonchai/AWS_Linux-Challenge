d4c5dae32ea4981f05b681c27b6e32b1f3103a07
highonchai <connor.high@gmail.com>
2020-10-07 12:47:39 -0700

    updates to masterfile logic and purchaseloop test file

--- purchaseloop_test.sh
+++ purchaseloop_test.sh
@@ -2,64 +2,72 @@
 
 cart_products=()
 cart_quantity=()
+products=(Greens Beans Potatoes Tomatoes Corn)
+prices=(1 2 3 4 5)
 
-#purchase() {
+purchase() {
 
-#}
-
-# Asks the user if they would like to add items or purchase
-# Purchasing Items loop
-more="yes"
-until [ $more = "no" ]; do
-read -p "What would you like to add to your cart?"
-read -p "Please select a Product ID: >> " product
-
-case $product in
-1)
 read -p "How many would you like? >> " quantity
-echo "$quantity $product added to your cart"
-cart_products+=$product
+cart_products+=${products[$(($product - 1))]}
 cart_quantity+=$quantity
+echo "$quantity ${products[$(($product - 1))]} added to your cart"
 sleep 0.25
 read -p "Would you like to add more items? (yes/no) >> " more
 
 if [ $more = "yes" ]
 then
 #(loop back to beginning of the process)
-continue
+return
 elif [ $more = "no" ]
 then
 clear
 echo "Taking you to the checkout page!"
 #loading()
 #checkout()
-exit
 else
 echo "Invalid response. Returning to menu"
-continue
-exit
+return
 fi
 
+}
+
+# Asks the user if they would like to add items or purchase
+# Purchasing Items loop
+more="yes"
+until [ $more = "no" ]; do
+echo "What would you like to add to your cart?"
+read -p "Please select a Product ID: >> " product
+
+case $product in
+1)
+purchase
 ;;
 2)
-echo "2"
-#purchase()
+#echo "2"
+purchase
 ;;
 3)
-echo "3"
-#purchase()
+#echo "3"
+purchase
 ;;
 4)
-echo "4"
-#purchase()
+#echo "4"
+purchase
 ;;
 5)
-echo "5"
-#purchase()
+#echo "5"
+purchase
 ;;
 esac
 
 done
 
-echo "${cart_products[0]}"
+for n in "${cart_products[@]}"; do
+printf "$n "
+done
+printf "\n"
 
+for n in "${cart_quantity[@]}"; do
+printf "$n "
+done
+printf "\n"
